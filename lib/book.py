0d0e9f3d9a5e8ed5f439ba86365df124894e0eca
Mariam Senzia <mariamsenzia@gmail.com>
2024-03-01 10:51:02 +0300

    classes,instances and properties implementation

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+       self.title = title
+       self._page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
     
-        
+    @page_count.setter
+    def page_count(self,new_page_count):
+        '''prints "page_count must be an integer" if page_count is not an integer.'''
+        if not isinstance(new_page_count, int):
+           print("page_count must be an integer")
+        else:
+            self._page_count = new_page_count
+            
+    # def set_book(self, page_count):
+    #     if not isinstance(page_count, int):
+    #        print("page_count must be an integer")
+    #     else:
+    #         self.page_count = page_count
+    #         return  
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!") 
+        return self    
+
+# book = Book('Psychology of money', 200)
+# print(book.title)
+# print(book.page_count)
+# print(book.turn_page())
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand = brand
+        self.size = size
+        self.condition = 'Used'
+
+    @property
+    def shoe_size(self):
+        return self.size
+    
+    @shoe_size.setter
+    def shoe_size(self,size):
+        if not isinstance(size, int):
+            print('size must be an integer')
+        else: 
+            self.size = size
+
+    def can_cobble(self):
+        print("Your shoe has been repaired!")
+        self.condition = 'New'
+        return self
